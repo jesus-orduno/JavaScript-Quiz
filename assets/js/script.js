e397c6e4a39c73d9e881cf9c41635eed00c43b7a
jesus-orduno <jesus.orduno@gmail.com>
2022-07-25 00:05:59 -0700

    clean up css and add questions and variables to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,163 @@
-// Variables for each character type
+var questions = [
+  {
+    questionText: 
+      "Inside which HTML element do we put the JavaScript?",
+    options: [
+      "1. <scripting>", 
+      "2. <javascript>", 
+      "3. <js>", 
+      "4. <script>"
+    ],
+    answer: "4. <script>",
+  },
+  {
+    questionText: 
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    options: [
+      "1. <script src='xxx.js'>",
+      "2. <script name='xxx.js'>",
+      "3. <script href='xxx.js'>",
+      "4. <script file='xxx.js'>"
+    ],
+    answer: "1. <script src='xxx.js'>",
+  },
+  {
+    questionText:
+      "How can you add a comment in JavaScript?",
+    options: [
+      "1. <!--This is a comment-->", 
+      "2. //This is a comment", 
+      "3. <!>This is a comment", 
+      "4. <!-This is a comment"
+    ],
+    answer: "2. //This is a comment",
+  },
+  {
+    questionText:
+      "How do you declare a JavaScript variable?",
+    options: [
+      "1. var myName = 'John';",
+      "2. let variable myName = 'John';",
+      "3. variable is myName = 'John';",
+      "4. v myName = 'John';"
+    ],
+    answer: "1. var myName = 'John';",
+  },
+  {
+    questionText:
+      "Which operator is used to assign a value to a variable?",
+    options: [
+      "1. x", 
+      "2. *", 
+      "3. =", 
+      "4. -"
+    ],
+    answer: "3. =",
+  },
+  {
+    questionText:
+      "What will the following code return? Boolean(10 > 9)",
+    options: [
+      "1. NaN", 
+      "2. true", 
+      "3. false", 
+      "4. undefined"
+    ],
+    answer: "2. true",
+  },
+  {
+    questionText:
+      "How do you write 'Hello World' in an alert box?",
+    options: [
+      "1. msgBox('Hello World');", 
+      "2. alert('Hello World');", 
+      "3. msg('Hello World');", 
+      "4. alertBox('Hello World');"
+    ],
+    answer: "2. alert('Hello World');",
+  },
+  {
+    questionText:
+      "How do you create a function in JavaScript?",
+    options: [
+      "1. function = myFunction()", 
+      "2. myFunction()", 
+      "3. function: myFunction()", 
+      "4. function myFunction()"
+    ],
+    answer: "4. function myFunction()",
+  },
+  {
+    questionText:
+      "How do you round the number 7.25, to the nearest integer?",
+    options: [
+      "1. Math.round(7.25)", 
+      "2. round(7.25)", 
+      "3. Math.rnd(7.25)", 
+      "4. rnd(7.25)"
+    ],
+    answer: "1. Math.round(7.25)",
+  },
+  {
+    questionText: 
+      "Commonly used data types DO NOT include:",
+    options: [
+      "1. strings", 
+      "2. booleans", 
+      "3. alerts", 
+      "4. numbers"
+    ],
+    answer: "3. alerts",
+  },
+  {
+    questionText: 
+      "Arrays in JavaScript can be used to store ______.",
+    options: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above"
+    ],
+    answer: "4. all of the above",
+  },
+  {
+    questionText:
+      "String values must be enclosed within _____ when being assigned to variables.",
+    options: 
+      ["1. commas", 
+      "2. curly brackets", 
+      "3. quotes", 
+      "4. parentheses"
+    ],
+    answer: "3. quotes",
+  },
+  {
+    questionText:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    options: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console.log"
+    ],
+    answer: "4. console.log",
+  },
+  {
+    questionText:
+      "Which of the following is a statement that can be used to terminate a loop, switch or label statement?",
+    options: [
+      "1. break", 
+      "2. stop", 
+      "3. halt", 
+      "4. exit"
+    ],
+    answer: "1. break",
+  },
+];
 
-// Links to generate id element in HTML
+var startCard = document.querySelector("start-card");
+var quizCard = document.querySelector("quiz-card");
+var scoreCard = document.querySelector("score-card");
+var leaderboardCard = document.querySelector("leaderboard-card");
 
-// Adds event listener to generate button
 
